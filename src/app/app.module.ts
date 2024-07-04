07d63b535d55ab5459fab340bd46dc22e18957e1
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-07-04 08:23:14 +0200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+export const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatFormFieldModule,
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/constants/categories.ts
@@ -0,0 +1,8 @@
+import { Option } from "./option";
+
+export const CATEGORIES: Option[] = [
+  { value: 'science', name: 'Science' },
+  { value: 'history', name: 'History' },
+  { value: 'music', name: 'Music' },
+  { value: 'sport', name: 'Sport' }
+];
